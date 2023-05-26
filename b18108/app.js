fd852355c16e06eb051dd250a30bd254fc6067df
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2023-05-26 23:31:12 +0900

    10430-1

---
+++ b18108/app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = parseInt(input);
+
+console.log(input - 543);
