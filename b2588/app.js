0bd668143e3fd46dab96adaa91db92af14f3fa5f
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2023-05-26 23:02:41 +0900

    b2588-1

--- b2588/app.js
+++ b2588/app.js
@@ -3,11 +3,16 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
 input = input.map((item) => +item);
-input = input[0];
 
 solution(input[0], input[1]);
 
 function solution(A, B) {
     const stringB = String(B);
     const one = +stringB[2];
+    const ten = +stringB[1];
+    const hundred = +stringB[0];
+    console.log(A * one);
+    console.log(A * ten);
+    console.log(A * hundred);
+    console.log(A * B);
 }
