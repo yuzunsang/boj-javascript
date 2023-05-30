1337a9d1bde4f025c3101f11b825a68224f2a17d
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2023-05-31 06:58:43 +0900

    10951-1 trying

--- b10951/app.js
+++ b10951/app.js
@@ -2,3 +2,11 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
+input = input[0];
+input = input.split(' ').map((item) => +item);
+console.log(input);
+
+for (let i = 0 ; i < input.length ; i++) {
+    input = +input[i];
+    input = input.split(' ').map((item) => +item);
+}
