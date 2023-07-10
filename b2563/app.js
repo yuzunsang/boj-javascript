6edd2a00797bb3bf522ef2e3e30affef2e7933a2
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2023-07-11 00:44:16 +0900

    2563 - 1

--- b2563/app.js
+++ b2563/app.js
@@ -1,24 +1,38 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+const readline = require("readline");
 
-let arr = Array.from(Array(100), () => Array(100).fill(false));
-const n = +input[0];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-presolution(n);
+let input = [];
 
-function presolution(n) {
-    let data = [];
+rl.on("line", (line) => {
+    input.push(line.toString().split('').map((item) => +item));
 
-    for (let i = 1; i <= n; i++) {
-        data.push(+input[i]);
+}).on('close', () => {
+    input.shift();
+    let arr = Array.from(Array(100), () => Array(100).fill(false));
+
+    for (let i = 0; i < input.length; i++) {
+        let x = input[i][0];
+        let y = input[i][1];
+
+        for (let j = 0; j < 10; j++) {
+            for (let k = 0; k < 10; k++) {
+                arr[x + j][y + k] = true;
+            }
+        }
     }
 
-    console.log(data);
-}
+    let result = arr.reduce((acc, curr) => {
+        for (let el of curr) {
+            if (el) acc++;
+        }
 
-// solution(n, data);
-
-// function solution(n, data) {
-
-// }
+        return acc;
+    }, 0);
+    
+    console.log(result);
+    process.exit();
+});
