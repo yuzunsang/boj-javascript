ba4d13b5a2991083564f070f52aeaa72038b5aaa
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2024-01-22 08:06:40 +0900

    dp - 1로 바꾸기

---
+++ b1463/app.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = +input[0];
+const dp = new Array(n + 1).fill(0);
+
+for (let i = 2; i <= n; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+}
+
+console.log(dp[n]);
